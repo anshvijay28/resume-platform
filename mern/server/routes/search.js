51d3c22ee50cc80b32352b2a6afe4a959ff4f6f4
Mrinal <cmrinal16@gmail.com>
2022-12-13 14:06:14 -0500

    super basic app functionality

--- mern/server/routes/search.js
+++ mern/server/routes/search.js
@@ -1,42 +1,59 @@
 // This will help us connect to the database
 const dbo = require("../db/conn");
 const Realm = require("realm");
-const realmApp = new Realm.App({id: "resume-platform-miyzy"})
+const realmApp = new Realm.App({id: process.env.APP_ID});
+const {verifyClientToken} = require("./admin");
 
-exports.search = (req, res) => {
-    if (req.body.searchText == null || req.body.searchText == "") {
-        res.json({message: "Please enter a search term", status: "ERROR"});
+exports.search = async (req, res) => {
+    if (req.body.search === undefined || req.headers.accesstoken === undefined) {
+        res.status(400).send("Missing required fields");
         return;
     }
-    let db_connect = dbo.getDb();
-    db_connect
-        .collection("resumes")
-        .find({})
-        .toArray()
-        .then((resumes) => {
-            var hits = [];
-            resumes.forEach((resume) => {
-                let rawText = resume.resumes[0].data.rawText.toLowerCase()
-                if (rawText.includes(req.body.searchText.toLowerCase())) {
-                    hits.push(resume.resumes[0].data);
-                }
-            });
-
-            let results = [];
-            hits.forEach((hit) => {
-                let result = {
-                    name: hit.name.raw,
-                    email: hit.emails[0],
-                    linkedin: hit.linkedin
-                }
-                results.push(result);
-            });
-            res.json({message: "Successfully searched", status: "SUCCESS", results: results});
-
-        })
-        .catch((err) => {
-            res.json(err);
-        });
+    verifyClientToken(req.headers.accesstoken)
+    .then( (result) => {
+        if (result.action !== "PASS") {
+            res.status(401).send(result.action);
+            return;
+        } else if (result.action === "PASS") {
+            if (req.body.search == null || req.body.search == "") {
+                res.status(401).send("Please enter a search term");
+                return;
+            }
+            let db_connect = dbo.getDb();
+            db_connect
+                .collection("resumes")
+                .find({})
+                .toArray()
+                .then((resumes) => {
+                    var hits = [];
+                    resumes.forEach((resume) => {
+                        let rawText = resume.resumes[0].data.rawText.toLowerCase()
+                        if (rawText.includes(req.body.search.toLowerCase())) {
+                            hits.push(resume.resumes[0].data);
+                        }
+                    });
+        
+                    let results = [];
+                    hits.forEach((hit) => {
+                        let result = {
+                            name: hit.name.raw,
+                            email: hit.emails[0],
+                            linkedin: hit.linkedin
+                        }
+                        results.push(result);
+                    });
+                    res.json({message: "Successfully searched", results: results});
+        
+                })
+                .catch((err) => {
+                    res.json(err);
+                });
+        }
+    })
+    .catch( (err) => {
+        res.json(err);
+    });
+    
 }
 
 
--- mern/server/server.js
+++ mern/server/server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/index"));
+const {getAdminKey} = require("./routes/admin");
 // get driver connection
 const dbo = require("./db/conn");
 
 app.listen(port, () => {
+  getAdminKey();
+  
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
@@ -17,3 +20,4 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
