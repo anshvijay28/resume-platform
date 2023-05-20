a2f1d27c054ea8876265b81b6cadf7402ec009e6
Ansh Vijay <anshvijay28@gmail.com>
2023-05-20 18:43:00 -0400

    changing res.sendFile to accept all paths

--- mern/server/server.js
+++ mern/server/server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
 });
 
 app.use(require("./routes/index"));
