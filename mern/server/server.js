766bbf45b83869f1ab3ec35abbe868c7784b2092
Ansh Vijay <anshvijay28@gmail.com>
2023-05-28 17:47:49 -0400

    cleaned up server.js a little

--- mern/server/server.js
+++ mern/server/server.js
@@ -7,14 +7,10 @@ app.use(bodyParser.json());
 require("dotenv").config({ path: path.join(__dirname, 'config.env') });
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-// app.use(cors({
-//   origin: "http://localhost:3000"
-// }));
 app.use(cors());
 app.use(express.json());
 
-//only do these 2 commands when in prod
-if (port != 5000) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', function (req, res) {
